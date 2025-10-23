9fe74a987883b1814db055eb6b050bacd08204d1
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-10-23 00:15:23 -0300

    update: utilizando icones sem preenchimento

--- lib/data/models/helpguide_model.dart
+++ lib/data/models/helpguide_model.dart
@@ -28,13 +28,13 @@ class HelpGuide {
   static IconData _getIconFromName(String? name) {
     switch (name) {
       case 'local_fire_department':
-        return Icons.local_fire_department;
+        return Icons.local_fire_department_outlined;
       case 'cardiology':
-        return Icons.heart_broken;
+        return Icons.heart_broken_outlined;
       case 'flood':
-        return Icons.flood;
+        return Icons.flood_outlined;
       case 'local_police':
-        return Icons.local_police;
+        return Icons.local_police_outlined;
       default:
         return Icons.help_outline;
     }
